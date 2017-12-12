5225a7d06b1660866e3b78f628346cb0874983c3
johanna fulghum <johanna.fulghum@gmail.com>
2017-12-11 20:44:34 -0500

    Completed sometimes function

---
+++ 03_sometimes/sometimes.js
@@ -0,0 +1,18 @@
+
+function sometimes(func){
+  count = 0;
+  return function (){
+    while (count < 3){
+      count++;
+      return func.apply(this, arguments)
+    } 
+    while (count >= 3 && (count % 2 === 0)){
+      count++
+      return func.apply(this, arguments)
+    } 
+     while (count >= 3 && (count % 2 !== 0)){
+      count++
+      return "I do not know!";
+    } 
+  }
+}
