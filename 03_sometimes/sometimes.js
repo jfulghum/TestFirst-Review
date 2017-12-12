0aaece41fa506919cd77e81444a873a1dc461a66
johanna fulghum <johanna.fulghum@gmail.com>
2017-12-11 20:46:17 -0500

    refactor sometimes function

--- 03_sometimes/sometimes.js
+++ 03_sometimes/sometimes.js
@@ -2,11 +2,7 @@
 function sometimes(func){
   count = 0;
   return function (){
-    while (count < 3){
-      count++;
-      return func.apply(this, arguments)
-    } 
-    while (count >= 3 && (count % 2 === 0)){
+    while (count < 3 || (count >= 3 && (count % 2 === 0))){
       count++
       return func.apply(this, arguments)
     } 
