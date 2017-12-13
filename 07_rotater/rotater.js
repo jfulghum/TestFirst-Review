d895e9c672dd22f5908968c34e6a70c39224e08c
johanna fulghum <johanna.fulghum@gmail.com>
2017-12-12 23:34:00 -0500

    complete

--- 07_rotater/rotater.js
+++ 07_rotater/rotater.js
@@ -1,9 +1,27 @@
-function rotater(string){
-  return function rotate(num){
-    if (num > string.length){
-      rotated = num % string.length
-      string.slice((string.length - rotated), string.length).concat(string.slice(0, rotated))
-    }
-    return (string.slice(string.length - num, string.length))
-  }
+// function rotater(string){
+//   return function rotate(num){
+//     if (num > string.length){
+//       rotated = num % string.length
+//       string.slice((string.length - rotated), string.length).concat(string.slice(0, rotated))
+//     }
+//     return string.slice(0, num).concat(string.slice(string.length - num, string.length))
+//   }
+// }
+
+function rotater(str) {
+	var flip = false,
+		rotatedStr;
+
+	return function(steps){
+		if(steps === str.length) {
+			flip = !flip;
+		}
+		if(!flip) {
+			rotatedStr = str.slice(steps) + str.slice(0,steps);
+		} else {
+			rotatedStr = str.slice(str.length - steps) + str.slice(0, str.length-steps);
+		}
+		
+		return rotatedStr;
+	};
 }
